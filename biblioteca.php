3aeb766c5921453520d316ceb22f1aec0c1c1a5e
yeduke17 <87336622+yeduke17@users.noreply.github.com>
2021-08-10 22:11:44 -0500

    Add files via upload

---
+++ biblioteca.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto</title>
+    <link href="Estilos.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+
+
+
+<body background="images/fondo.jpg">
+    <div class="container">
+        <a href="images PB/interfaz principal.docx">ir al documento</a>
+		   <nav>
+						<a href="nosotros.php"<button type="button" style="margin:10px" class="btn btn-warning">Nosotros</button></a>
+						<a href="exeriencias.php"<button type="button" style="margin:10px" class="btn btn-success">EXPERIENCIAS</button></a>
+						<a href="plataformaEducativa.php"<button type="button" style="margin:10px" class="btn btn-danger">PLATAFORMA EDUCATIVA</button></a>
+						<a href="contacto.php"<button type="button" style="margin:10px" class="btn btn-warning">CONTACTO</button></a>
+
+                    </nav>
+
+        
+            <div class="row text-center align-items-center">
+                <div class="col-3">
+                    <div>
+                        <br> <br>
+							
+                        <div align="center"> <img class="mx-auto d-block" src="images/logo.jpeg" title="hecho por yessica" width="500" height="500">
+						</div>
+                        <br> <br>
+                    </div>
+                </div>
+                <div class="col-9" style="margin-top: 80px">
+                 
+                </div>
+            </div>
+
+</body>
+        
+
+
+
+
+        
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+
+</html>
